cdb1c85b1ed9fae555adac59ea9293e05d81ad66
yawok <obengky72@gmail.com>
2022-02-17 22:24:08 +0000

    added multiple picture tweeting.

--- tweeter.py
+++ tweeter.py
@@ -18,20 +18,21 @@ import twi_tions as t
 import config
 
 
-COLLECT, TWEET, INTERACT = range(3)
+images = []
+COLLECT_CAP, NO_OF_IMGS, COLLECT_IMG, CONFIRM, TWEET_PHOTO, TWEET, INTERACT = range(7)
 def start(update: Update, context: CallbackContext) -> int:
     """Bot introduction and conversation initialisation"""
     #Storing chat id to send messages wihtout user input later
     global chat_id
     chat_id = update.message.chat_id
-    reply_keyboard = [['Yes', 'No']]
+    reply_keyboard = [['Caption only', 'Caption with images']]
     update.message.reply_text("Hello, welcome to Twitter By yaw.o.k."
             "Do you want to tweet?", reply_markup=RKM(
                 reply_keyboard, one_time_keyboard=True, input_field_placeholder="yes or no?"
                 )
             )
     
-    return COLLECT
+    return COLLECT_CAP
 
 
 def collect_tweet(update: Update, context: CallbackContext) -> int:
@@ -48,6 +49,86 @@ def tweet(update: Update, context: CallbackContext) -> int:
     
     global sent
     sent = t.send_tweet(user_in)
+    if sent:
+        update.message.reply_text("Tweet successfully sent.")
+    else: 
+        update.message.reply_text("Tweet not sent.")
+    
+    #like or retweet
+    keyboard = [
+            [
+                IKB("Like", callback_data="1"),
+                IKB("Retweet", callback_data="2")
+                ],
+            [IKB("Done", callback_data="0")]
+            ]
+
+    reply_markup = IKM(keyboard)
+
+    update.message.reply_text("Would you like to retweet or like?", reply_markup=reply_markup)
+
+    return INTERACT
+
+
+def collect_cap(update: Update, context: CallbackContext) -> int:
+    """Collect caption for images"""
+    update.message.reply_text("Enter the caption for the tweet.")
+
+    return NO_OF_IMGS
+
+ 
+def no_of_imgs(update: Update, context: CallbackContext) -> int:
+    """Collect images."""
+    global caption 
+    caption = update.message.text
+
+    reply_keyboard = [['1', '2'], ['3', '4']]
+    update.message.reply_text("How many images do you want to attach to the tweet?",
+            reply_markup=RKM(
+                reply_keyboard, one_time_keyboard=True, input_field_placeholder="1"
+                )
+            )
+    
+    return COLLECT_IMG
+
+
+def collect_img(update: Update, context: CallbackContext) -> int:
+    """Collect images."""
+    global no_imgs 
+    no_imgs = int(update.message.text)
+
+    update.message.reply_text(f"Send {no_imgs} image(s) to attach to tweet.")
+    
+    return CONFIRM
+
+
+def confirm(update: Update, context: CallbackContext) -> int:
+    """Ask for confirmation to tweet."""
+    global images
+    photo = update.message.photo[-1]
+    if update.message.photo:
+        img = photo.get_file().download(f"images/img{len(images)}.jpg")
+        images.append(img)
+        print(len(images))
+        
+        if no_imgs - len(images) != 0:
+            return CONFIRM
+
+    reply_keyboard = [['Yes', 'No']]
+    update.message.reply_text("Do you want to send tweet with images",
+            reply_markup=RKM(
+                reply_keyboard, one_time_keyboard=True, input_field_placeholder="yes or no?"
+                )
+            )
+    
+    return TWEET_PHOTO
+
+def tweet_img(update: Update, context: CallbackContext) -> int:
+    """Send tweet."""
+    user_in = update.message.text
+    
+    global sent
+    sent = t.send_tweet(caption, imgs=images)
     if sent:
         update.message.reply_text("Tweet successfully sent.")
     else: 
@@ -98,7 +179,7 @@ def button(update: Update, context: CallbackContext) -> int:
                 )
             )
         
-    return COLLECT
+    return COLLECT_CAP
 
 
 def help(update: Update, context: CallbackContext) -> None:
@@ -116,8 +197,15 @@ def main() -> None:
     
     conv = ConversationHandler(entry_points = [CommandHandler("start", start)],
             states ={
-                COLLECT: [
-                    MessageHandler(Filters.regex('^(Yes)$'), collect_tweet), 
+                COLLECT_CAP: [
+                    MessageHandler(Filters.regex('^(Caption only)$'), collect_tweet), 
+                    MessageHandler(Filters.regex('^(Caption with images)$'), collect_cap)
+                    ],
+                NO_OF_IMGS: [MessageHandler(Filters.text, no_of_imgs)],
+                COLLECT_IMG: [MessageHandler(Filters.text, collect_img)],
+                CONFIRM: [MessageHandler(Filters.photo, confirm)],
+                TWEET_PHOTO:[
+                    MessageHandler(Filters.regex('^(Yes)$'), tweet_img),
                     MessageHandler(Filters.regex('^(No)$'), start)
                     ],
                 TWEET: [MessageHandler(Filters.text, tweet)],
--- twi_tions.py
+++ twi_tions.py
@@ -11,9 +11,16 @@ auth = OAuth1UserHandler(
 
 api = API(auth)
 
-def send_tweet(this) -> int:
-    """Sends tweets to twitter account."""
-    tweet = api.update_status(status = this)
+def send_tweet(this, imgs=[]) -> int:
+    """Sends tweets and images."""
+    media_ids = []
+    if imgs:
+        for img in imgs:
+            res = api.media_upload(img)
+            media_ids.append(res.media_id)
+
+    tweet = api.update_status(status=this, media_ids=media_ids)
+
     print(f'Tweeted\n{tweet.id}\nto your account.')
     
     return tweet.id
@@ -31,3 +38,4 @@ def like(tweet_id) -> int:
     api.create_favorite(tweet_id)
 
     return 1
+
